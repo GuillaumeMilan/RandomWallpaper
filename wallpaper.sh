f1e92243974d126ef55b417ba1e3cb6753c64ba3
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-04-10 14:32:59 +0200

    retire message de wget

--- wallpaper.sh
+++ wallpaper.sh
@@ -12,7 +12,7 @@ mot=$(echo $var | sed -n "s/<\/div>//p")
 rm *
 adressegoogle="https://www.google.fr/search?q=wallpaper+$mot&safe=off&biw=1920&bih=880&tbs=isz:ex,iszw:1920,iszh:1080&tbm=isch&source=lnt"
 firefox $adressegoogle
-wget --no-cache --no-cookie --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0" --referer="www.google.com" -e robots=off  $adressegoogle #&>/dev/null
+wget -q --no-cache --no-cookie --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0" --referer="www.google.com" -e robots=off  $adressegoogle #&>/dev/null
 ls
 #firefox *
 #echo $adressegoogle
